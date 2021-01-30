aa180b82f071528c68f8487df2af97398da8da41
MatsyshynAnton <anton.matsyshyn@outlook.com>
2021-01-30 15:08:29 +0200

    Add DAL project to solution

--- YourNews/YourNews.WebAPI/Startup.cs
+++ YourNews/YourNews.WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YourNews.DAL;
 
 namespace YourNews.WebAPI
 {
@@ -26,7 +27,6 @@ namespace YourNews.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
