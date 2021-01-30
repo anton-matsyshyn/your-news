416f76d061a83bab23fa432016c14df5c8fa0aaa
Raynor Jim <gkmaes1r0@gmail.com>
2021-01-30 20:37:59 +0200

    Add dbcontext to startup

--- YourNews/YourNews.WebAPI/Startup.cs
+++ YourNews/YourNews.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using YourNews.DAL.Contexts;
 
 namespace YourNews.WebAPI
 {
@@ -27,6 +29,8 @@ namespace YourNews.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<YourNewsContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:YourNews"]));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "YourNews.WebAPI", Version = "v1" });
